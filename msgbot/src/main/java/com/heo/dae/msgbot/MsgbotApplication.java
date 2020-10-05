108f1901bb182d7ad4630ec9741a8af8981dbe23
dev09 <devdevdevdevdev09@gmail.com>
2020-10-05 11:10:46 +0900

    add project

---
+++ msgbot/src/main/java/com/heo/dae/msgbot/MsgbotApplication.java
@@ -0,0 +1,13 @@
+package com.heo.dae.msgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgbotApplication.class, args);
+	}
+
+}
