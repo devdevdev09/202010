5789ddc165cb8b8436413699a0a5dbc737c704d0
dev09 <devdevdevdevdev09@gmail.com>
2020-10-07 13:22:32 +0900

    우선 순위 추가

--- msgbot/src/main/java/com/heo/dae/msgbot/controller/MainController.java
+++ msgbot/src/main/java/com/heo/dae/msgbot/controller/MainController.java
@@ -1,20 +1,67 @@
 package com.heo.dae.msgbot.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import com.heo.dae.msgbot.enums.Messengers;
+import com.heo.dae.msgbot.messenger.Kakaotalk;
+import com.heo.dae.msgbot.messenger.Line;
 import com.heo.dae.msgbot.messenger.Slack;
+import com.heo.dae.msgbot.messenger.Telegram;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
+    // @Autowired
+    // Kakaotalk kakaotalk;
+
     @Autowired
     Slack slack;
 
-    @RequestMapping("/test")
-    @ResponseBody
+    // @Autowired
+    // Line line;
+
+    // @Autowired
+    // Telegram Telegram;
+
+    @Value("${priority.list}")
+    List<Messengers> list;
+
+    // @PostMapping("/send")
+    @GetMapping("/send")
+    // public void test(@RequestBody Map<String, Object> requestBody){
     public void test(){
-        slack.send("test");
+        // String msg = (String)requestBody.get("msg");
+        // 설정한 우선순위로 메시지 발송
+
+        // 순회 방법 고민....
+        for(Messengers messenger : list){
+            if(messenger.equals(Messengers.KAKAOTALK)){
+                System.out.println("카카오톡 발송");
+            }else{
+                if(messenger.equals(Messengers.SLACK)){
+                    System.out.println("슬랙 발송");
+                }else{
+                    if(messenger.equals(Messengers.LINE)){
+                        System.out.println("라인 발송");
+                    }else{
+                        if(messenger.equals(Messengers.TELEGRAM)){
+                            System.out.println("텔레그램 발송");
+                        }else{
+                            System.out.println("발송 X");
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 }
--- msgbot/src/main/java/com/heo/dae/msgbot/enums/Messengers.java
+++ msgbot/src/main/java/com/heo/dae/msgbot/enums/Messengers.java
@@ -1,7 +1,7 @@
 package com.heo.dae.msgbot.enums;
 
 public enum Messengers {
-    SLACK("슬랙"),KAKAO("카카오톡"),LINE("라인"),TELE("텔레그램");
+    SLACK("슬랙"),KAKAOTALK("카카오톡"),LINE("라인"),TELEGRAM("텔레그램");
 
     private String name;
 
