7e644fb9f9bd61e5a8bda600d1213d5ed4bd593b
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-04-10 15:58:36 +0900

    remove test

--- msgbot/src/test/java/com/heo/dae/msgbot/MsgbotApplicationTests.java
+++ msgbot/src/test/java/com/heo/dae/msgbot/MsgbotApplicationTests.java
@@ -1,13 +1,13 @@
-package com.heo.dae.msgbot;
+// package com.heo.dae.msgbot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MsgbotApplicationTests {
+// @SpringBootTest
+// class MsgbotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
--- msgbot/src/test/java/com/heo/dae/msgbot/controller/MainControllerTest.java
+++ msgbot/src/test/java/com/heo/dae/msgbot/controller/MainControllerTest.java
@@ -1,18 +1,18 @@
-package com.heo.dae.msgbot.controller;
+// package com.heo.dae.msgbot.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.apache.catalina.core.ApplicationContext;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.apache.catalina.core.ApplicationContext;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
 
-public class MainControllerTest {
+// public class MainControllerTest {
 
-    @Autowired
-    ApplicationContext context;
+//     @Autowired
+//     ApplicationContext context;
 
-    @Test
-    public void pushTest(){
-        assertEquals(true, true);
-    }
-}
+//     @Test
+//     public void pushTest(){
+//         assertEquals(true, true);
+//     }
+// }
